ce7eafdb080d40aa465c3f297cc57a2bd0c61dff
sean <pulpfictionost@gmail.com>
2021-12-15 02:30:35 +0000

    Day 6

---
+++ ruby/day_6.rb
@@ -0,0 +1,19 @@
+data = File.readlines("data/day_6")
+
+state = data[0].split(",").map { |e| e.to_i }
+p state
+(1..80).each { |i|
+  new_fish = 0
+  state.map! { |fish|
+    days = fish - 1
+    if days < 0
+      new_fish += 1
+      6
+    else
+      days
+    end
+  }
+  1.upto(new_fish).each { |nf| state.append(8) }
+}
+
+p state.size
